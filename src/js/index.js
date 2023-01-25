18f64ed2ec26f25ffe6b962dc6b1164580aebd42
Pedro Pinheiro <pedropereira0604@gmail.com>
2023-01-25 20:34:37 -0300

    Update projeto-mario

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+const botaoTrailer = document.querySelector(".botao-trailer")
+console.log(botaoTrailer)
+
+botaoTrailer.addEventListener("click", () => {
+    console.log("clicou no botão veja o trailer");
+})
