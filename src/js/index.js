4ae5d43cae595ca8a87c3c98d23b054c34c2aabe
Pedro Pinheiro <pedropereira0604@gmail.com>
2023-01-26 22:33:04 -0300

    Update com Java-Script.

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,22 @@
-const botaoTrailer = document.querySelector(".botao-trailer")
-console.log(botaoTrailer)
+const botaoTrailer = document.querySelector(".botao-trailer");
+const botaoFecharModal = document.querySelector(".fechar-modal");
+const video = document.getElementById("video");
+const modal = document.querySelector(".modal");
+const linkDoVideo = video.src;
+
+function alternarModal(){
+    modal.classList.toggle("aberto");
+}
 
 botaoTrailer.addEventListener("click", () => {
-    console.log("clicou no botão veja o trailer");
+    alternarModal();
+    video.setAttribute("src", linkDoVideo);
 })
+
+botaoFecharModal.addEventListener("click", () => {
+     alternarModal();
+    video.setAttribute("src", "");
+})
+
+
+
